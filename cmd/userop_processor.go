a0297bd09bf81f211af8865f191a1a2277657206
Mario Karagiorgas <salem8@gmail.com>
2024-11-17 16:29:08 +0200

    fix: regen signature when len is 65

--- cmd/userop_processor.go
+++ cmd/userop_processor.go
@@ -3,7 +3,7 @@ package cmd
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/hex"
 	"fmt"
 	"math/big"
 
@@ -83,7 +83,7 @@ func (p *UserOpProcessor) ProcessUserOps(userOps []*model.UserOperation, submiss
 	}
 	fmt.Printf("Entrypoint handleOps callData: \n%s\n\n", callData)
 
-	if len(userOps) == 1 && userOps[0].Signature != nil && len(userOps[0].Signature) == 132 {
+	if len(userOps) == 1 && userOps[0].Signature != nil && len(userOps[0].Signature) == 65 {
 		// applicable only for single UserOperation
 		// TODO: Verify multi ops signature
 		p.verifyOpSig(chainIDs[0], p.Signer, userOps[0])
